b5d2c5fc71e1152363534a89141bab7fd14d5d3a
Abhishek Kumar <abhishekkr03.work@gmail.com>
2024-07-29 02:34:57 +0530

    Game Version 1.2

--- Tic-Tac-Toe/src/com/game/LaunchGame.java
+++ Tic-Tac-Toe/src/com/game/LaunchGame.java
@@ -1,5 +1,6 @@
 package com.game;
 
+import java.util.Random;
 import java.util.Scanner;
 
 class TicTacToe {
@@ -69,21 +70,17 @@ class TicTacToe {
 	}
 }
 
-class HumanPlayer{
+
+
+abstract class Player{
 	
 	String name;
 	char mark;
 	
-	public HumanPlayer(String name, char mark) {
-		super();
-		this.name = name;
-		this.mark = mark;
-	}
+	abstract void makeMove();
 	
-	static boolean isValidMove(int row, int column) {
-		
+	boolean isValidMove(int row, int column) {
 		if(row >= 0 && row <= 2 && column >= 0 && column <= 2) {
-			
 			if(TicTacToe.board[row][column] == ' ') {
 				return true;
 			}
@@ -91,23 +88,7 @@ class HumanPlayer{
 		return false;
 	}
 	
-	void makeMove() {
-		
-		@SuppressWarnings("resource")
-		Scanner scan = new Scanner(System.in);
-		int row;
-		int column;
-		do {
-			System.out.println("Enter the Row");
-			row = scan.nextInt();
-			System.out.println("Enter the Column");
-			column = scan.nextInt();
-		} while (!isValidMove(row, column));
-		
-		TicTacToe.placeMark(row, column, mark);		
-	}
-	
-	static boolean isBoardFull() {
+	static boolean checkDraw() {
 	    for (int i = 0; i < TicTacToe.board.length; i++) {
 	        for (int j = 0; j < TicTacToe.board[i].length; j++) {
 	            if (TicTacToe.board[i][j] == ' ') {
@@ -119,43 +100,147 @@ class HumanPlayer{
 	}
 }
 
+class HumanPlayer extends Player{
+		
+	public HumanPlayer(String name, char mark) {
+		super();
+		this.name = name;
+		this.mark = mark;
+	}
+	
+	void makeMove() {
+		
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		int row = -1;
+		int column = -1;
+		do {
+			System.out.println("Please enter the row (0, 1, or 2) where you want to place your mark:");
+	        row = scan.nextInt();
+	        System.out.println("Please enter the column (0, 1, or 2) where you want to place your mark:");
+	        column = scan.nextInt();
+	        
+	        if (!isValidMove(row, column)) {
+	            System.out.println("Invalid move, Please enter a valid row and column.");
+			}
+		} while (!isValidMove(row, column));
+		
+		TicTacToe.placeMark(row, column, mark);		
+	}
+}
+
+
+class AIPlayer extends Player{
+	
+	public AIPlayer(String name, char mark) {
+		super();
+		this.name = name;
+		this.mark = mark;
+	}
+	
+	void makeMove() {
+		
+		@SuppressWarnings({ "resource", "unused" })
+		Scanner scan = new Scanner(System.in);
+		int row;
+		int column;
+		do {
+			Random random = new Random();
+			row = random.nextInt(3);
+			column = random.nextInt(3);
+		} while (!isValidMove(row, column));
+		
+		TicTacToe.placeMark(row, column, mark);		
+	}	
+}
+
+
 public class LaunchGame {
 	@SuppressWarnings({ "unused", "resource" })
 	public static void main(String[] args) {
 		TicTacToe t = new TicTacToe();
 		
 		Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to Tic-Tac-Toe!");
-        System.out.println("Please enter the name of Player 1:");
-        String player1 = scan.next();
-        System.out.println("Now, please enter the name of Player 2:");
-        String player2 = scan.next();
-		
-		HumanPlayer p1 = new HumanPlayer(player1, 'X');
-		HumanPlayer p2 = new HumanPlayer(player2, 'O');
-		
-		HumanPlayer cp;
-		cp = p1;
-		
-		TicTacToe.displayBoard();
+        System.out.println("Welcome to Tic-Tac-Toe! \n");
+        
+        System.out.println("Please select a game mode: \nPress 1 for Player vs Player \nPress 2 for Player vs Computer");
+        int gameMode = scan.nextInt();
 
-		while(true) {
-			System.out.println(cp.name + ", it's your turn! Make your move.");
-			cp.makeMove();
-			TicTacToe.displayBoard();
-			if(TicTacToe.checkRowWin() || TicTacToe.checkColWin() || TicTacToe.checkDiagWin()) {
-				System.out.println("Congratulations " + cp.name + "! You have won the game!");
-				break;
-			}else if(HumanPlayer.isBoardFull()) {
-				System.out.println("Game Over - It's a tie!");
-                break;
-			}else {
-				if(cp == p1) {
-					cp = p2;
+        Player cp;
+        
+        //For Human Players
+        if (gameMode == 1) {
+	        System.out.println("\nPlease enter the name of Player 1:");
+	        String player1 = scan.next();
+	        System.out.println("Now, please enter the name of Player 2:");
+	        String player2 = scan.next();
+	
+	        HumanPlayer p1 = new HumanPlayer(player1, 'X');
+	        HumanPlayer p2 = new HumanPlayer(player2, 'O');
+	
+	        cp = p1;
+	
+	        TicTacToe.displayBoard();
+	
+	        while (true) {
+	            System.out.println(cp.name + ", it's your turn! Make your move.");
+	            cp.makeMove();
+	            TicTacToe.displayBoard();
+	
+	            if (TicTacToe.checkRowWin() || TicTacToe.checkColWin() || TicTacToe.checkDiagWin()) {
+	                System.out.println("Congratulations " + cp.name + "! You have won the game!");
+	                break;
+	            } else if (HumanPlayer.checkDraw()) {
+	                System.out.println("Game Over - It's a tie!");
+	                break;
 				}else {
-					cp = p1;
+					if(cp == p1) {
+						cp = p2;
+					}else {
+						cp = p1;
+					}
 				}
 			}
-		}
+        }
+        
+        //For AI Player
+        else if (gameMode == 2) {
+        	System.out.println("\nPlease enter your name:");
+	        String player1 = scan.next();
+	
+	        HumanPlayer p1 = new HumanPlayer(player1, 'X');
+	        AIPlayer p2 = new AIPlayer("BOB", 'O');
+	
+	        cp = p1;
+	
+	        TicTacToe.displayBoard();
+	
+	        while (true) {
+	        	if(cp == p1) {	
+	        		System.out.println(cp.name + ", it's your turn! Make your move.");
+	        	}else {
+	        		System.out.println(cp.name + ": Here is my move.");
+	        	}
+	        	
+	            cp.makeMove();
+	            TicTacToe.displayBoard();
+	
+	            if (TicTacToe.checkRowWin() || TicTacToe.checkColWin() || TicTacToe.checkDiagWin()) {
+	                System.out.println("Congratulations " + cp.name + "! You have won the game!");
+	                break;
+	            } else if (AIPlayer.checkDraw()) {
+	                System.out.println("Game Over - It's a tie!");
+	                break;
+				}else {
+					if(cp == p1) {
+						cp = p2;
+					}else {
+						cp = p1;
+					}
+				}
+			}
+        } else {
+            System.out.println("Invalid selection. Please restart the game and choose a valid mode.");
+        }
 	}
 }
